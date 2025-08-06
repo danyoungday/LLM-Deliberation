8c2ddc4ea765e48db93c9ba2c1366b60dea3428b
Daniel Young <danyoung@utexas.edu>
2025-08-05 17:53:45 -0700

    Ran black and renamed coordinator to moderator

--- mystuff/moderator_round_prompts.py
+++ mystuff/moderator_round_prompts.py
@@ -1,7 +1,22 @@
-from prompt_utils import format_history, build_first_slot
+"""
+Prompts for the moderator per round.
+"""
+from prompt_utils import build_first_slot, format_history
 
-class CoordinatorRoundPrompts:
-    def __init__(self,agent_name, p1_name, initial_deal, incentive=None, scratch_pad_function=None, window_size=6, target_agent='', rounds_num=24, agents_num=6):
+
+class ModeratorRoundPrompts:
+    def __init__(
+        self,
+        agent_name,
+        p1_name,
+        initial_deal,
+        incentive=None,
+        scratch_pad_function=None,
+        window_size=6,
+        target_agent="",
+        rounds_num=24,
+        agents_num=6,
+    ):
         self.agent_name = agent_name
         self.p1_name = p1_name
         self.incentive = incentive
@@ -13,19 +28,25 @@ class CoordinatorRoundPrompts:
         self.agents_num = agents_num
 
     def build_slot_prompt(self, history, round_idx, other_args={}):
-        first = (round_idx == 0)  # first round
-        final_round = ((self.rounds_num-round_idx) <= self.agents_num)  # final time the agent would speak
-        final_vote = (round_idx == self.rounds_num)  # final voting session
+        first = round_idx == 0  # first round
+        final_round = (
+            self.rounds_num - round_idx
+        ) <= self.agents_num  # final time the agent would speak
+        final_vote = round_idx == self.rounds_num  # final voting session
 
-        if first and self.p1_name == self.agent_name: # start the negotiation by P1's deal
+        if (
+            first and self.p1_name == self.agent_name
+        ):  # start the negotiation by P1's deal
             return build_first_slot(self.initial_deal, self.p1_name)
 
         # get history
-        history_prompt = self.get_history_input(history, final_round=final_round, final_vote=final_vote)
+        history_prompt = self.get_history_input(
+            history, final_round=final_round, final_vote=final_vote
+        )
 
         # get scratchpad by incentive
         scratch_pad = ""
-        if self.incentive == 'cooperative':
+        if self.incentive == "cooperative":
             scratch_pad = self.cooperative_scratch_pad()
         elif self.scratch_pad_function:
             scratch_pad = self.scratch_pad_function(other_args)
@@ -34,20 +55,23 @@ class CoordinatorRoundPrompts:
         unified_instructions = self.get_unified_instructions()
 
         # prompt agent to generate plan
-        plan_prompt = self.get_plan_prompt(self.agent_name == self.p1_name, final_round, final_vote)
+        plan_prompt = self.get_plan_prompt(
+            self.agent_name == self.p1_name, final_round, final_vote
+        )
 
         # collate
-        slot_prompt = history_prompt + scratch_pad + unified_instructions + plan_prompt 
+        slot_prompt = history_prompt + scratch_pad + unified_instructions + plan_prompt
 
         return slot_prompt
 
-
-    def get_history_input(self, history, final_round=False,final_vote=False):
-        personalized_history, last_plan = format_history(self.agent_name, history,self.window_size) 
-        slot_prompt = f"The following is a chronological history of up to {self.window_size} interactions <HISTORY> {personalized_history} </HISTORY> "      
+    def get_history_input(self, history, final_round=False, final_vote=False):
+        personalized_history, last_plan = format_history(
+            self.agent_name, history, self.window_size
+        )
+        slot_prompt = f"The following is a chronological history of up to {self.window_size} interactions <HISTORY> {personalized_history} </HISTORY> "
 
         if last_plan:
-            slot_prompt += f"The following are your previous plans from last interactions. You should follow them while also adjusting them according to new observations. <PREV_PLAN> {last_plan} </PREV_PLAN> "      
+            slot_prompt += f"The following are your previous plans from last interactions. You should follow them while also adjusting them according to new observations. <PREV_PLAN> {last_plan} </PREV_PLAN> "
 
         slot_prompt += "\n Now it is your turn to talk."
 
@@ -56,28 +80,28 @@ class CoordinatorRoundPrompts:
         elif final_vote:
             slot_prompt += " This is an official and final voting session."
         return slot_prompt
-        
+
     def get_unified_instructions(self):
-        prompt = ''' 
+        prompt = """ 
         Enclose the scratchpad between <SCRATCHPAD> and </SCRATCHPAD>. The scratchpad is secret and not seen by other parties.
         Your final answer is public and must never contain scores. Enclose your final answer after the scratchpad between <ANSWER> and </ANSWER>.
         Make your final answer very short and brief in 2-3 sentences and containing only your main proposals.
         Use options' short notations instead of long descriptions.
         Enclose the exact name of the party you propose to speak next between: <PARTY> </PARTY>. "
-        '''
+        """
         return prompt
 
     def get_plan_prompt(self, is_p1, final_round, final_vote):
-        plan_prompt = ''
+        plan_prompt = ""
         if not final_round:
-            plan_prompt = ''' 
+            plan_prompt = """ 
             After the final answer, building on your current move and analysis,
             briefly write down short notes for yourself of what exact options you can explore the next time you speak.
-            Enclose the notes between <PLAN> and </PLAN>. '''
-        return plan_prompt 
+            Enclose the notes between <PLAN> and </PLAN>. """
+        return plan_prompt
 
-    def cooperative_scratch_pad(self):        
-        scratch_pad = ''' 
+    def cooperative_scratch_pad(self):
+        scratch_pad = """ 
         Please use a scratchpad to show intermediate calculations and explain yourself and why you are choosing a certain party to speak next. 
         In your scratchpad, 
             1) think about what others may prefer,
@@ -89,6 +113,6 @@ class CoordinatorRoundPrompts:
         Express your objectives clearly and actively listen to others.
         Empathize with other parties' concerns to foster rapport.
         Focus on common interests to create a win-win situation.
-        It is very important for you that they all reach an agreement and their minimum scores are met.'''
+        It is very important for you that they all reach an agreement and their minimum scores are met."""
 
-        return scratch_pad 
+        return scratch_pad
